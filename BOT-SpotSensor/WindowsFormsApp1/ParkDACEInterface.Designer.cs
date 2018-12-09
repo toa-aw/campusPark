94a5ee26b23a63ce2d6c7f128b6bcd7e9dd199f6
Sebastian Altamirano <2161610@my.ipleiria.pt>
2018-12-09 11:17:43 +0000

    ParkDACE implementado. Falta mejorar la interfaz gráfica y utilizar el worker en la parte del BOT.

--- BOT-SpotSensor/WindowsFormsApp1/ParkDACEInterface.Designer.cs
+++ BOT-SpotSensor/WindowsFormsApp1/ParkDACEInterface.Designer.cs
@@ -32,36 +32,33 @@
             this.txtBoxFormatedData = new System.Windows.Forms.TextBox();
             this.lblFormatedData = new System.Windows.Forms.Label();
             this.lblRecieved = new System.Windows.Forms.Label();
-            this.label1 = new System.Windows.Forms.Label();
-            this.textBoxXmlFile = new System.Windows.Forms.TextBox();
-            this.button1 = new System.Windows.Forms.Button();
-            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
             this.btnFormatData = new System.Windows.Forms.Button();
             this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
             this.SuspendLayout();
             // 
             // txtBoxRecievedData
             // 
-            this.txtBoxRecievedData.Location = new System.Drawing.Point(13, 86);
+            this.txtBoxRecievedData.Location = new System.Drawing.Point(13, 30);
             this.txtBoxRecievedData.Margin = new System.Windows.Forms.Padding(4);
             this.txtBoxRecievedData.Multiline = true;
             this.txtBoxRecievedData.Name = "txtBoxRecievedData";
-            this.txtBoxRecievedData.Size = new System.Drawing.Size(417, 404);
+            this.txtBoxRecievedData.Size = new System.Drawing.Size(417, 476);
             this.txtBoxRecievedData.TabIndex = 0;
             // 
             // txtBoxFormatedData
             // 
-            this.txtBoxFormatedData.Location = new System.Drawing.Point(515, 86);
+            this.txtBoxFormatedData.Location = new System.Drawing.Point(552, 13);
             this.txtBoxFormatedData.Margin = new System.Windows.Forms.Padding(4);
             this.txtBoxFormatedData.Multiline = true;
             this.txtBoxFormatedData.Name = "txtBoxFormatedData";
-            this.txtBoxFormatedData.Size = new System.Drawing.Size(422, 181);
+            this.txtBoxFormatedData.Size = new System.Drawing.Size(1182, 40);
             this.txtBoxFormatedData.TabIndex = 1;
             // 
             // lblFormatedData
             // 
             this.lblFormatedData.AutoSize = true;
-            this.lblFormatedData.Location = new System.Drawing.Point(512, 65);
+            this.lblFormatedData.Location = new System.Drawing.Point(438, 30);
             this.lblFormatedData.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblFormatedData.Name = "lblFormatedData";
             this.lblFormatedData.Size = new System.Drawing.Size(106, 17);
@@ -71,46 +68,16 @@
             // lblRecieved
             // 
             this.lblRecieved.AutoSize = true;
-            this.lblRecieved.Location = new System.Drawing.Point(13, 65);
+            this.lblRecieved.Location = new System.Drawing.Point(10, 9);
             this.lblRecieved.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblRecieved.Name = "lblRecieved";
             this.lblRecieved.Size = new System.Drawing.Size(105, 17);
             this.lblRecieved.TabIndex = 4;
             this.lblRecieved.Text = "Recieved Data:";
             // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(16, 32);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(122, 17);
-            this.label1.TabIndex = 5;
-            this.label1.Text = "Configuration File:";
-            // 
-            // textBoxXmlFile
-            // 
-            this.textBoxXmlFile.Location = new System.Drawing.Point(145, 32);
-            this.textBoxXmlFile.Name = "textBoxXmlFile";
-            this.textBoxXmlFile.Size = new System.Drawing.Size(712, 22);
-            this.textBoxXmlFile.TabIndex = 6;
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(863, 29);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 29);
-            this.button1.TabIndex = 7;
-            this.button1.Text = "Open";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // openFileDialog1
-            // 
-            this.openFileDialog1.FileName = "openFileDialog1";
-            // 
             // btnFormatData
             // 
-            this.btnFormatData.Location = new System.Drawing.Point(841, 509);
+            this.btnFormatData.Location = new System.Drawing.Point(19, 521);
             this.btnFormatData.Name = "btnFormatData";
             this.btnFormatData.Size = new System.Drawing.Size(96, 23);
             this.btnFormatData.TabIndex = 8;
@@ -122,21 +89,28 @@
             // 
             this.listBox1.FormattingEnabled = true;
             this.listBox1.ItemHeight = 16;
-            this.listBox1.Location = new System.Drawing.Point(515, 284);
+            this.listBox1.Location = new System.Drawing.Point(437, 60);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(422, 196);
+            this.listBox1.Size = new System.Drawing.Size(1298, 196);
             this.listBox1.TabIndex = 9;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 16;
+            this.listBox2.Location = new System.Drawing.Point(437, 278);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(1298, 228);
+            this.listBox2.TabIndex = 10;
             // 
             // ParkDACEInterface
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(956, 556);
+            this.ClientSize = new System.Drawing.Size(1747, 556);
+            this.Controls.Add(this.listBox2);
             this.Controls.Add(this.listBox1);
             this.Controls.Add(this.btnFormatData);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.textBoxXmlFile);
-            this.Controls.Add(this.label1);
             this.Controls.Add(this.lblRecieved);
             this.Controls.Add(this.lblFormatedData);
             this.Controls.Add(this.txtBoxFormatedData);
@@ -144,6 +118,7 @@
             this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "ParkDACEInterface";
             this.Text = "ParkDACE";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -155,12 +130,9 @@
         private System.Windows.Forms.TextBox txtBoxFormatedData;
         private System.Windows.Forms.Label lblFormatedData;
         private System.Windows.Forms.Label lblRecieved;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox textBoxXmlFile;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.OpenFileDialog openFileDialog1;
         private System.Windows.Forms.Button btnFormatData;
         private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
     }
 }
 
--- BOT-SpotSensor/WindowsFormsApp1/ParkDACEInterface.cs
+++ BOT-SpotSensor/WindowsFormsApp1/ParkDACEInterface.cs
@@ -11,89 +11,133 @@ using WindowsFormsApp1.ServiceRefParkingSpotBot;
 using System.Xml;
 using System.Xml.Schema;
 using BOT_SpotSensor;
+using System.IO;
+using System.Collections;
+using uPLibrary.Networking.M2Mqtt;
 
 namespace WindowsFormsApp1
 {
     public partial class ParkDACEInterface : Form
     {
-        //private Timer timer1;
-        private List<ParkingSpot> parkingSpotsList;
+        const String STR_CHANNEL_NAME = "parkingSpots";
+
+        //MqttClient m_cClient = new MqttClient(IPAddress.Parse("192.168.237.155"));
+        MqttClient m_cClient;
+        string[] m_strTopicsInfo = { STR_CHANNEL_NAME };
+
+
+        XmlDocument parkingSpotsDoc = new XmlDocument();
         private BackgroundWorker bw = new BackgroundWorker();
         private ParkingSensorNodeDll.ParkingSensorNodeDll dll = null;
-        private List<string> spotsDLL = new List<string>();
 
-        public void DoWork(object sender, DoWorkEventArgs e)
-        {
-            dll.Initialize(NewSensorValueFunction, 50);
-        }
-
+        Hashtable spotsLocations = new Hashtable();
 
         public ParkDACEInterface()
         {
             InitializeComponent();
-            bw.DoWork += new DoWorkEventHandler(DoWork);
+            dll = new ParkingSensorNodeDll.ParkingSensorNodeDll();            
+        }
 
-            dll = new ParkingSensorNodeDll.ParkingSensorNodeDll();
-            bw.RunWorkerAsync();
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (m_cClient.IsConnected)
+            {
+                m_cClient.Unsubscribe(m_strTopicsInfo); //Put this in a button to see notif!
+                m_cClient.Disconnect(); //Free process and process's resources
+            }
         }
 
         public void NewSensorValueFunction(string str)
-        {
+        {            
             //To have access to the listbox that is in other thread (Form)
             this.BeginInvoke((MethodInvoker)delegate
             {
-                txtBoxRecievedData.Text += str + Environment.NewLine;
-                spotsDLL.Add(str);
+                XmlElement newspot = null;
+                string[] spotData = str.Split(';');
+                for (int i = 0; i < spotData.Length; i++)
+                {
+                    txtBoxRecievedData.Text += spotData[i] + Environment.NewLine;
+                }
+
+                switch (spotData[3])
+                {
+                    case "0":
+                        newspot = createParkingSpot(parkingSpotsDoc, spotData[0], "ParkingSpot", spotData[1], spotsLocations[spotData[1]].ToString(), "free", spotData[2], spotData[4]);
+                        sendSpotData(newspot);
+                        break;
+                    case "1":
+                        newspot = createParkingSpot(parkingSpotsDoc, spotData[0], "ParkingSpot", spotData[1], spotsLocations[spotData[1]].ToString(), "occupied", spotData[2], spotData[4]);
+                        sendSpotData(newspot);
+                        break;
+                    default:
+                        break;
+                }              
+                
+                listBox1.Items.Add(newspot.OuterXml);
+                
                 dll.Stop();
-            });
-        }
+            });            
+        }        
 
         public void readConfigFile()
         {
-            XmlDocument doc = new XmlDocument();
-            doc.Load(textBoxXmlFile.Text);
+            XmlDocument configDoc = new XmlDocument();
+            string path = "ParkingNodesConfig.xml";
+            configDoc.Load(path);
 
-            XmlElement root = doc.DocumentElement;
+            XmlElement configRootElem = configDoc.DocumentElement;
 
-            string unit = root.GetAttribute("units");
+            string unit = configRootElem.GetAttribute("units");
+            string delay = configRootElem.GetAttribute("refreshRate");
 
-            switch (unit)
-            {
-                default:
-                    break;
-            }
-
-
-
-            XmlNodeList lst = doc.SelectNodes("//provider/connectionType");
+            XmlNodeList lst = configDoc.SelectNodes("//provider");
             foreach (XmlNode node in lst)
             {
-                switch (node.InnerText)
+                XmlElement connectionType = node["connectionType"];
+                XmlElement parkInfo = node["parkInfo"];
+                int numberOfSpots = 0;
+
+                switch (connectionType.InnerText)
                 {
                     case "DLL":
-                        foreach (string item in spotsDLL)
-                        {
-                            string[] spot = item.Split(';');
-                            //listBox1.Items.Add(spot[1]);
-                        }
                         
+                        numberOfSpots = int.Parse(parkInfo["numberOfSpots"].InnerText);
+                        loadExcelData(parkInfo["geoLocationFile"].InnerText, numberOfSpots);
+                        
+                        switch (unit)
+                        {
+                            case "seconds":
+                                int delaysec = int.Parse(delay) * 1000;
+                                dll.Initialize(NewSensorValueFunction, delaysec);
+                                break;
+                            case "minutes":
+                                int delaymin = int.Parse(delay) * 60000;
+                                dll.Initialize(NewSensorValueFunction, delaymin);
+                                break;
+                            default:
+                                break;
+                        }
+
                         break;
                     case "SOAP":
                         using (BotServiceClient service = new BotServiceClient())
                         {
-                            //XmlDocument doc2 = new XmlDocument();
-                            //string xml = service.GetSpotsString();
-                            //doc.LoadXml(xml);
-                            //XmlNodeList spots = doc.SelectNodes("/parkinglot/parkingSpot");
-                            //foreach (XmlNode item in spots)
-                            //{
-                            //    listBox1.Items.Add(item["name"].InnerText);
-                            //}
-                            //List<ParkingSpot> parkingSpots = service.GetSpotsData().ToList();
-                            //foreach (ParkingSpot spot in parkingSpots)
-                            //{
-                            //    listBox1.Items.Add(spot);
-                            //}
+                            numberOfSpots = int.Parse(parkInfo["numberOfSpots"].InnerText);
+                            loadExcelData(parkInfo["geoLocationFile"].InnerText, numberOfSpots);
+                            //txtBoxFormatedData.Text = parkInfo["geoLocationFile"].InnerText;
+                            XmlDocument botParkingSpotsDoc = new XmlDocument();
+                            string xml = service.GetSpotsString();
+                            botParkingSpotsDoc.LoadXml(xml);
+                            XmlNodeList spots = botParkingSpotsDoc.SelectNodes("/parkinglot/parkingSpot");
+                            
+                            foreach (XmlNode item in spots)
+                            {
+                                XmlNode status = item["status"];
+                                string location = spotsLocations[item["name"].InnerText].ToString();
+                                XmlElement spot = createParkingSpot(parkingSpotsDoc, item["id"].InnerText, item["type"].InnerText, item["name"].InnerText, location, status["value"].InnerText, status["timestamp"].InnerText, item["batteryStatus"].InnerText);
+                                sendSpotData(spot);
+                                listBox2.Items.Add(spot.OuterXml);
+                            }
                         }
                         break;
                     default:
@@ -102,20 +146,72 @@ namespace WindowsFormsApp1
             }
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        public void loadExcelData(string filename, int linesToRead)
         {
-            openFileDialog1.InitialDirectory = Application.StartupPath;
-            openFileDialog1.Filter = "xml files (*.xml)|*.xml";
-
-            if (openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            int i = 0;
+            int range = linesToRead + 5;
+            List<string> parkIds = ExcelLib.ExcelHandler.ReadNxMCells(filename, "A6", "A"+ range);
+            List<string> spotLocations = ExcelLib.ExcelHandler.ReadNxMCells(filename, "B6", "B" + range);
+            
+            
+            foreach (string key in parkIds)
             {
-                textBoxXmlFile.Text = openFileDialog1.FileName;
+                if (!spotsLocations.Contains(key))
+                {
+                    spotsLocations.Add(key, spotLocations[i]);
+                    i++;
+                }
             }
+
+        }
+
+        public void sendSpotData(XmlElement pakingSpot)
+        {
+            m_cClient = new MqttClient("127.0.0.1");
+            m_cClient.Connect(Guid.NewGuid().ToString());
+            if (!m_cClient.IsConnected)
+            {
+                MessageBox.Show("Error connecting to message broker...");
+                return;
+            }
+            m_cClient.Publish(STR_CHANNEL_NAME, Encoding.UTF8.GetBytes(pakingSpot.OuterXml));
         }
 
         private void btnFormatData_Click(object sender, EventArgs e)
         {
             readConfigFile();
+        }
+
+        public XmlElement createParkingSpot(XmlDocument doc, string parkid, string spotType, string spotname, string spotlocation, string spotstatusvalue, string spotTimestamp, string spotbatterystatus)
+        {
+            XmlElement parkingSpot = doc.CreateElement("parkingSpot");
+
+            XmlElement id = doc.CreateElement("id");
+            id.InnerText = parkid;
+            XmlElement type = doc.CreateElement("type");
+            type.InnerText = spotType;
+            XmlElement name = doc.CreateElement("name");
+            name.InnerText = spotname;
+            XmlElement location = doc.CreateElement("location");
+            location.InnerText = spotlocation;
+            XmlElement status = doc.CreateElement("status");
+            XmlElement value = doc.CreateElement("value");
+            value.InnerText = spotstatusvalue;
+            XmlElement timestamp = doc.CreateElement("timestamp");
+            timestamp.InnerText = spotTimestamp;
+            status.AppendChild(value);
+            status.AppendChild(timestamp);
+            XmlElement batteryStatus = doc.CreateElement("batteryStatus");
+            batteryStatus.InnerText = spotbatterystatus;
+
+            parkingSpot.AppendChild(id);
+            parkingSpot.AppendChild(type);
+            parkingSpot.AppendChild(name);
+            parkingSpot.AppendChild(location);
+            parkingSpot.AppendChild(status);
+            parkingSpot.AppendChild(batteryStatus);
+
+            return parkingSpot;
         }
     }
 }
